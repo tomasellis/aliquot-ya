527680d37544eb9afede97500b1c455259a7f19f
Tomás Ellis <tomasitoellis@gmail.com>
2021-07-29 21:10:44 -0300

    chore: remove log

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,5 @@ createConnection()
     console.log("Loading users from the database...");
     const users = await connection.manager.find(User);
     console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
   })
   .catch((error) => console.log(error));
